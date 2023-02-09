a3298526151f7b4697e52a5c5f44603353dcfb3b
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-02-09 20:30:15 +0530

    Add files via upload

---
+++ code_62.java
@@ -0,0 +1,54 @@
+import java.util.*;
+ 
+public class code_62 {
+ 
+    static boolean areBracketsBalanced(String expr)
+    {
+        Deque<Character> stack
+            = new ArrayDeque<Character>();
+ 
+        for (int i = 0; i < expr.length(); i++) {
+            char x = expr.charAt(i);
+ 
+            if (x == '(' || x == '[' || x == '{') {
+               
+                stack.push(x);
+                continue;
+            }
+ 
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+            case ')':
+                check = stack.pop();
+                if (check == '{' || check == '[')
+                    return false;
+                break;
+ 
+            case '}':
+                check = stack.pop();
+                if (check == '(' || check == '[')
+                    return false;
+                break;
+ 
+            case ']':
+                check = stack.pop();
+                if (check == '(' || check == '{')
+                    return false;
+                break;
+            }
+        }
+ 
+        return (stack.isEmpty());
+    }
+    public static void main(String[] args)
+    {
+        String expr = "([{}])";
+ 
+        if (areBracketsBalanced(expr))
+            System.out.println("Balanced ");
+        else
+            System.out.println("Not Balanced ");
+    }
+}
