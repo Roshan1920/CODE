6281b238794ec0bd012a07e753d7988df5e34ce2
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-02-15 23:01:36 +0530

    Add files via upload

---
+++ code_67.java
@@ -0,0 +1,13 @@
+public class code_67
+{
+    public static void main(String[] args)
+    {
+        String str1 = "Code Sixty sEVEN        is a QualityAna    list";
+  
+        String str2 = str1.replaceAll("\\s", "");
+  
+        System.out.println(str2);
+  
+           }
+}
+    
