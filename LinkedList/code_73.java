96e68c9819c5224261fde4f5a9e4873ba319db47
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-04-14 23:17:12 +0530

    Add files via upload

---
+++ LinkedList/code_73.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+public class code_73
+{
+public static void main(String args[])
+{
+    LinkedList <String> l1 = new LinkedList<String>();
+    l1.add("A");
+    l1.add("M");
+    l1.addFirst("R");
+
+    System.out.println(l1);
+}
+}
