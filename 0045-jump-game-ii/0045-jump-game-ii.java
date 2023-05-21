0dee5e3910b4e375934f190a397fcd2b7580f9b8
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-05-21 14:38:09 +0530

    Time: 1 ms (99.58%), Space: 44.7 MB (5.23%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump=0;
+        int pos= 0;
+        int des = 0;
+        
+        for(int i=0;i<nums.length-1;i++)
+        {
+            //no need to check last element
+            
+            des= Math.max(des,nums[i]+i);
+            
+            if(pos==i){
+              pos=des;
+                jump++;
+            }
+       
+        }
+         return jump;
+
+    }
+}
