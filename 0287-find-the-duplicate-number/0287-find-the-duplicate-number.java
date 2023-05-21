1589b58087c4842209d8cd5f63d6202e9ace9788
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-05-21 18:00:28 +0530

    Time: 3 ms (92.60%), Space: 56.5 MB (94.99%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+     for(int i=0;i<nums.length;i++)
+     {
+       int index = Math.abs(nums[i]);
+         //incase index value is negative it eill besome positive with abs
+         
+         if(nums[index]<0) return index;
+         // agar dobaaro koi negative hua to sidha return 
+         
+         nums[index] = -nums[index];
+         //
+     }
+        return -1;
+    }
+}
