faa6251a32781188c48f181247175ef8b1696a65
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-05-31 17:48:27 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find pairs with given sum in doubly linked list/find-pairs-with-given-sum-in-doubly-linked-list.java
@@ -0,0 +1,166 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node next,prev;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+        prev = null;
+    }
+    
+    public static Node inputList(BufferedReader br) throws IOException
+    {
+        int n = Integer.parseInt(br.readLine().trim()); // Length of Linked List
+        
+        String[] s = br.readLine().trim().split(" ");
+        Node head = new Node(Integer.parseInt(s[0])), tail = head;
+        for(int i = 1; i < s.length; i++){
+            Node temp=new Node(Integer.parseInt(s[i]));
+            tail.next = temp;
+            tail.next.prev = tail;
+            tail = tail.next;
+        }
+        return head;
+    }
+    
+    public static void printList(Node node)
+    {
+        while (node != null)
+        { 
+    		System.out.print(node.data + " ");
+    		node = node.next; 
+    	}  
+    	System.out.println();
+    }
+}
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+        
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+        
+        return mat;
+    }
+    
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+    
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        if(m.size()==0){
+            System.out.println("-1");
+        }else{
+            for(var a : m)
+            {
+                System.out.print("(" + a.get(0) + "," + a.get(1) + ")" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int target; 
+            target = Integer.parseInt(br.readLine());
+            
+            
+            Node head = Node.inputList(br);
+            
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> res = obj.findPairsWithGivenSum(target, head);
+            
+            IntMatrix.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for singly Link List Node
+class Node
+{
+    int data;
+    Node next,prev;
+    
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+
+You can also use the following for printing the link list.
+Node.printList(Node node);
+*/
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, Node head) {
+        // code here
+        Node tail=head;
+        ArrayList<ArrayList<Integer>> arr=new ArrayList<ArrayList<Integer>>();
+        while(tail.next!=null){
+            tail=tail.next;
+        }
+        //first traverse to the last element.
+        //now from last element anf first element compare them.
+        //find their sum and check it to the target value.
+        
+        while(head!=tail){
+            int sum=head.data+tail.data;
+            //if sum= target store in the array list.
+            if(sum==target){
+                ArrayList<Integer> arr1=new ArrayList<Integer>();
+                arr1.add(head.data);
+                arr1.add(tail.data);
+                arr.add(arr1);
+                head=head.next;
+            }
+            // if sum greater than target,
+            // then in order to reduce value go back from higher element to next higher and compare.
+            else if(sum>target){
+                tail=tail.prev;
+            }
+             //  if sum less than target then, to increase value go next from start.
+            else{
+                head=head.next;
+            }
+        }
+        return arr;
+
+    }
+}
+        
