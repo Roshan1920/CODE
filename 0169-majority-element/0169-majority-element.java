44e1ca9b9b39fd58710ae03239db3f22d3701986
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-06-05 22:48:46 +0530

    Time: 1 ms (99.63%), Space: 48.5 MB (18.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,20 +2,25 @@ class Solution {
     public int majorityElement(int[] nums) {
      
         int count=0;
-        int a=0;
+        int curr=0;
+        //count to keep  track of the count of the current majority element
+        //curr to variable stores the current majority element candidate.
         for(int i=0;i<nums.length;i++){
             if(count==0){
-                a=nums[i];
+                curr=nums[i];
             }
             
-            if(nums[i]==a){
+            if(nums[i]==curr){
                 count++;
+                // increment the count variable because we found another occurrence of the candidate majority element.
             }
             else{
                 count--;
+                //nums[i] is not equal to curr , decrement the count variable because we have found a different element.
             }
         }
-        return a;
+        return curr;
+        //return the value of curr as the majority element.
     
     }
 }
