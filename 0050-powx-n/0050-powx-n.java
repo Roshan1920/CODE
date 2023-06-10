6886be340d2980e734cc1a876db1a3b88a5273c4
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-06-10 22:17:45 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (92.39%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,9 +1,30 @@
 class Solution {
     public double myPow(double x, int n) {
-      if(n == 0) return 1;
-    if(n < 0) {
-       return ((1 / x) * (myPow(1 / x, -(n + 1))));
-    } 
-    return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2); 
-    }  
+
+        // Check if x is within the given constraints
+        if (x < -10000 || x > 10000) {
+            return 0;
+        }
+        
+        // Base condition: If n is 0, x^0 is 1
+        if (n == 0) {
+            return 1;
+        }
+
+        // If n is negative, take the reciprocal of x and make n positive
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+
+        // If n is even, recursively compute the square of x^(n/2)
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        } 
+        
+        // If n is odd, recursively compute x^(n-1) and multiply it by x
+        else {
+            return x * myPow(x, n - 1);
+        }
     }
+}
