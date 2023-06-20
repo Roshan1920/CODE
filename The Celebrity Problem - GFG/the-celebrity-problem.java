fb39267e1e848d8a459c4a7bfcdbe1174b81f14d
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-06-20 19:40:32 +0530

    Added solution - LeetHub

---
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int N = sc.nextInt();
+            int M[][] = new int[N][N];
+            for(int i=0; i<N; i++)
+            {
+                for(int j=0; j<N; j++)
+                {
+                    M[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println(new Solution().celebrity(M,N));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{ 
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(int M[][], int n)
+    {
+    	
+ 
+        int i = 0, j = n - 1;
+        while (i < j) 
+        {
+            if (M[j][i] == 1) // j knows i
+                j--;
+            else // j doesnt know i so i cant be celebrity
+                i++;
+        }
+        // i points to our celebrity candidate
+        int candidate = i;
+ 
+        // Now, all that is left is to check that whether
+        // the candidate is actually a celebrity i.e: he is
+        // known by everyone but he knows no one
+        for (i = 0; i < n; i++) 
+        {
+            if (i != candidate) 
+            {
+                if (M[i][candidate] == 0
+                    || M[candidate][i] == 1)
+                    return -1;
+            }
+        }
+        // if we reach here this means that the candidate
+        // is really a celebrity
+        return candidate;
+    }
+}
