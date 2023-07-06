21243d0617b1a20edce8dd7571ac9ebcd08d4d2f
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-07-06 09:45:21 +0530

    Added solution - LeetHub

---
+++ Replace every element with the least greater element on its right - GFG/replace-every-element-with-the-least-greater-element-on-its-right.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n; 
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.findLeastGreater(n, arr);
+            
+            IntArray.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+   public ArrayList<Integer> findLeastGreater(int len, int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        TreeSet<Integer> s  = new TreeSet<>();
+        
+         for(int i=len-1;i>=0; i--){
+            s.add(arr[i]);
+            Integer it = s.higher(arr[i]);
+            
+            if(it==null) ans.add(-1);
+            
+            else ans.add(it);
+        }
+        
+        Collections.reverse(ans);
+        
+        return ans;
+        
+    }
+}
+
+
+        
