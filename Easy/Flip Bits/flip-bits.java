867bd8cb0f9b90ec6ffe5ddf15a39f934725391f
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-08-15 23:36:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Flip Bits/flip-bits.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.maxOnes(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+
+public static int maxOnes(int a[], int n) {
+ int zero=0;
+int one=0;
+int max=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0){
+            zero++;
+        }
+        else{
+            one++;
+            zero--;
+        }
+          max=Math.max(max,zero);
+       if(zero<0){
+            zero=0;
+        }
+    }
+    
+    return one+max;
+       
+    }
+}
