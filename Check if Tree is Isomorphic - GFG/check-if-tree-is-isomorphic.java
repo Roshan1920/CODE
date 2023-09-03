663f6e0b85c6c3ede51bb501ba46bbd120390447
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-09-03 22:21:37 +0530

    Added solution - LeetHub

--- Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.java
+++ Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.java
@@ -119,24 +119,11 @@ class Solution
     // Return True if the given trees are isomotphic. Else return False.
     boolean isIsomorphic(Node root1, Node root2)  
     { 
-         if(root1==null && root2==null )
-         {
-             return true;
-         }
-         
-         if(root1==null || root2==null )
-         {
-             return false;
-         }
-         
-         if(root1.data !=root2.data)
-         {
-             return false;
-         }
-         boolean left = isIsomorphic(root1.left , root2.left) && isIsomorphic(root1.right , root2.right);
-         boolean right = isIsomorphic(root1.left,root2.right) &&isIsomorphic(root1.right,root2.left);
-         return left || right;
-         
+         // code here.
+           if(root1== null && root2 ==null) return true;
+        if((root1==null && root2!=null) || (root1 != null && root2==null)) return false;
+        if(root1.data != root2.data) return false;
+        return (isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right, root2.left)) || 
+        (isIsomorphic(root1.left, root2.left) && isIsomorphic(root1.right, root2.right));
     }
-    
-}    
+}
