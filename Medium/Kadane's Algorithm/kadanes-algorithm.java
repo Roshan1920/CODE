14d285e8909cf2795a210966758f601cbae92678
Roshan <89714795+Rehan2023@users.noreply.github.com>
2023-09-14 14:00:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    // arr: input array
+    // n: size of array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n) {
+        long maxsum = Integer.MIN_VALUE; // Initialize maxsum with the smallest possible value
+        long currsum = 0;
+
+        for (int i = 0; i < n; i++) {
+            currsum = currsum + arr[i];
+
+            if (currsum > maxsum) { // Changed ">=" to ">" here
+                maxsum = currsum;
+            }
+            if (currsum < 0) {
+                currsum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
+
+
